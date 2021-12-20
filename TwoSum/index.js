65e113dc2aa22f65897986c222b7832224f007bc
Jakub Widurek <jakubwidurek@gmail.com>
2021-12-20 21:30:39 +0100

    Two Sum O(n^2) version

--- TwoSum/index.js
+++ TwoSum/index.js
@@ -24,4 +24,19 @@
  * @return {number[]}
  */
 
-const twoSum = function (nums, target) {};
+// Time Complexity O(n^2)
+
+const twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    const numberToFind = target - nums[i];
+    for (let j = i + 1; j < nums.length; j++) {
+      if (numberToFind === nums[j]) {
+        return [i, j];
+      }
+    }
+  }
+
+  return null;
+};
+
+console.log(twoSum([2, 7, 11, 15], 9));
