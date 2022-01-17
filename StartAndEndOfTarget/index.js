3f9033b4b4f19384fcc6c32ddc108c750856b240
Jakub Widurek <jakubwidurek@gmail.com>
2022-01-17 10:48:10 +0100

    add binary search solution

--- StartAndEndOfTarget/index.js
+++ StartAndEndOfTarget/index.js
@@ -4,8 +4,6 @@
 
 // You must write an algorithm with O(log n) runtime complexity.
 
- 
-
 // Example 1:
 
 // Input: nums = [5,7,7,8,8,10], target = 8
@@ -18,7 +16,6 @@
 
 // Input: nums = [], target = 0
 // Output: [-1,-1]
- 
 
 // Constraints:
 
@@ -32,6 +29,42 @@
  * @param {number} target
  * @return {number[]}
  */
- var searchRange = function(nums, target) {
-    
+
+const binarySearch = (nums, left, right, target) => {
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    const midValue = nums[mid];
+    if (midValue === target) {
+      return mid;
+    } else if (midValue < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+};
+
+const searchRange = function (nums, target) {
+  if (nums.length < 1) return [-1, -1];
+  const midPosition = binarySearch(nums, 0, nums.length - 1, target);
+  if (midPosition === -1) return [-1, -1];
+  let startPosition = midPosition;
+  let endPosition = midPosition;
+  let temp1, temp2;
+
+  while (startPosition !== -1) {
+    temp1 = startPosition;
+    startPosition = binarySearch(nums, 0, startPosition - 1, target);
+  }
+  startPosition = temp1;
+
+  while (endPosition !== -1) {
+    temp2 = endPosition;
+    endPosition = binarySearch(nums, endPosition + 1, nums.length - 1, target);
+  }
+
+  endPosition = temp2;
+  return [startPosition, endPosition];
 };
