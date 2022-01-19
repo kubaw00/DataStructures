3ff7b8766a5692f8aecfbe6872130d04a94d1e1b
Jakub Widurek <jakubwidurek@gmail.com>
2022-01-19 22:27:29 +0100

    add breath for search version

--- BinaryTreeRightSideView/index.js
+++ BinaryTreeRightSideView/index.js
@@ -1,3 +1,15 @@
+// Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+// Example 1:
+
+// Input: root = [1,2,3,null,5,null,4]
+// Output: [1,3,4]
+// Example 2:
+
+// Input: root = [1,null,3]
+// Output: [1,3]
+// Example 3:
+
 // Input: root = []
 // Output: []
 
@@ -5,7 +17,7 @@
 
 // The number of nodes in the tree is in the range [0, 100].
 // -100 <= Node.val <= 100
-
+//
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
@@ -18,4 +30,28 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
-var rightSideView = function (root) {};
+
+/*--------------------Breath For Search version ------------------*/
+
+var rightSideView = function (root) {
+  if (!root) return [];
+
+  const queue = [root];
+  const res = [];
+
+  while (queue.length) {
+    let counter = 0;
+    let currentNode;
+    let length = queue.length;
+
+    while (counter < length) {
+      currentNode = queue.shift();
+
+      if (currentNode.left) queue.push(currentNode.left);
+      if (currentNode.right) queue.push(currentNode.right);
+      counter++;
+    }
+    res.push(currentNode.val);
+  }
+  return res;
+};
