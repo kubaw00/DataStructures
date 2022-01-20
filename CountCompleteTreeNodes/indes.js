08834f16018618c969c91ac468912ffb3d43bf66
Jakub Widurek <jakubwidurek@gmail.com>
2022-01-20 14:10:52 +0100

    add solution number of nodes in complete tree

--- CountCompleteTreeNodes/indes.js
+++ CountCompleteTreeNodes/indes.js
@@ -35,4 +35,55 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var countNodes = function (root) {};
+var countNodes = function (root) {
+  if (!root) return 0;
+
+  const height = getHeight(root);
+  if (height === 0) return 1;
+
+  const upperCount = Math.pow(2, height) - 1;
+
+  let left = 0,
+    right = upperCount;
+
+  while (left < right) {
+    let indexToFind = Math.ceil((left + right) / 2);
+    if (nodeExists(indexToFind, height, root)) {
+      left = indexToFind;
+    } else {
+      right = indexToFind - 1;
+    }
+  }
+
+  return upperCount + left + 1;
+};
+
+function nodeExists(indexToFind, height, node) {
+  let left = 0,
+    right = Math.pow(2, height) - 1,
+    count = 0;
+
+  while (count < height) {
+    let midOfNode = Math.ceil((left + right) / 2);
+    if (indexToFind >= midOfNode) {
+      node = node.right;
+      left = midOfNode;
+    } else {
+      node = node.left;
+      right = midOfNode - 1;
+    }
+    count++;
+  }
+
+  return node !== null;
+}
+
+function getHeight(node) {
+  const count = 0;
+  while (node.left !== null) {
+    node = node.left;
+    count++;
+  }
+
+  return count;
+}
